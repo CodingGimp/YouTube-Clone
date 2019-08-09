6b0c0c48eca87dcf9e259f6441c823bb6025a0aa
CodingGimp <coding.gimp@gmail.com>
2019-08-09 12:08:11 -0700

    Grid Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
+import { Grid } from '@material-ui/core'
 
+import YouTube from './api/youtube'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello</h1>
-            </div>
+            <Grid justify='center' container spacing={16}>
+                <Grid item xs={12}>
+                    <Grid container spacing={16}>
+                        <Grid item xs={12}>
+                        {/* SEARCH BAR */}
+                        </Grid>
+                        <Grid item xs={8}>
+                        {/* VIDEO DETAILS */}
+                        </Grid>
+                        <Grid item xs={4}>
+                        {/* VIDEO LIST */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
